a30fc549a2942a3e11b0efaa854f4b6e4640929b
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2019-03-04 18:44:44 +0100

    quasi acabat

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 #include<time.h>
 #include "SDL/include/SDL.h"
 using namespace std;
-
+#define speed 20;
+#define shotspeed 30;
 #pragma comment(lib, "SDL/SDL2.lib")
 #pragma comment(lib, "SDL/SDL2main.lib")
 
@@ -24,37 +25,71 @@ int main(int argc, char *argv[]) {
 
 	int x = 0;
 	int y = 0;
+	int sx = 0;
+	int sy = 0;
 	int multx = 2;
 	int multy = 2;
 
 	while (1) {
 		SDL_Event event;
 		while (SDL_PollEvent(&event)) {
-			
-				x = x + multx;
-				y = y + multy;
-			if (x >= 565) {
-				multx = -2;
-			}else if (x<=0){
-				multx = 2;
-			}
-			else if (y >= 430) {
-				multy = -2;
-			}
-			else if (y<=0) {
-				multy = 2;
+			switch (event.type) {
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.sym)
+				{
+				case SDLK_UP:
+					y = y - speed;
+					break;
+				case SDLK_DOWN:
+					y = y + speed;
+					break;
+				case SDLK_RIGHT:
+					x = x + speed;
+					break;
+				case SDLK_LEFT:
+					x = x - speed;
+					break;
+				case SDLK_SPACE:
+					sx = x+45;
+					sy = y + 25;
+					break;
+				case SDLK_ESCAPE:
+					return 0;
+				/*case (SDLK_LEFT&&SDLK_RIGHT):
+					x = x;
+					break;
+				case (SDLK_DOWN && SDLK_UP):
+					y = y;
+					break;
+					*/
+				default:
+					break;
+				}
+				break;
+			case SDL_QUIT:
+			return 0;
+			break;
 			}
 		}
+
+		sx = sx + shotspeed;
 		render = SDL_CreateRenderer(window, -1, 0);
 		SDL_SetRenderDrawColor(render, 0, 0, 100, 255);
 		SDL_RenderClear(render);
-		SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
 		SDL_Rect rectangle;
 		rectangle.x = x;
 		rectangle.y = y;
 		rectangle.w = 75;
 		rectangle.h = 50;
+		SDL_SetRenderDrawColor(render, 255, 0, 0, 255);
 		SDL_RenderFillRect(render, &rectangle);
+		SDL_Rect shot;
+		shot.x = sx;
+		shot.y = sy;
+		shot.w = 10;
+		shot.h = 5;
+		SDL_SetRenderDrawColor(render, 0, 200, 50, 255);
+		SDL_RenderFillRect(render, &shot);
 		SDL_RenderPresent(render);
 		SDL_DestroyRenderer(render);
 	}
