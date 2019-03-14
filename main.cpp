6f256650d44e3044eb444ced458de94f40033e79
marcpages2020 <47534017+marcpages2020@users.noreply.github.com>
2019-03-14 19:57:33 +0100

    it works

--- main.cpp
+++ main.cpp
@@ -11,27 +11,33 @@
 
 using namespace std;
 
-#define speed 2;
-#define shotspeed 30;
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+#define speed 2
+#define shotspeed 4
+#define SCREEN_WIDTH  640
+#define SCREEN_HEIGHT 480
 
 struct PlayerInput {
 	bool pressing_up = false;
 	bool pressing_down = false;
 	bool pressing_right = false;
 	bool pressing_left = false;
+	bool pressing_space = false;
 }player_input;
 
+Mix_Music *bacground_music = nullptr;
+Mix_Chunk *laser_beam = nullptr;
+
 int main(int argc, char *argv[]) {
+
 	SDL_Window *window; 
 	SDL_Renderer *render;
 	SDL_Surface *loading_surface;
-	SDL_Rect spaceship, background;
+	SDL_Rect spaceship, background,shot;
+
 	SDL_Init(SDL_INIT_EVERYTHING);
 	IMG_Init(IMG_INIT_PNG);
 	Mix_Init(MIX_INIT_OGG);
-	Mix_OpenAudio(MIX_DEFAULT_FREQUENCY,MIX_DEFAULT_FORMAT,2,8);
+	Mix_OpenAudio(MIX_DEFAULT_FREQUENCY,MIX_DEFAULT_FORMAT,2,1000);
 
 	int x = 0;
 	int y = 0;
@@ -40,6 +46,11 @@ int main(int argc, char *argv[]) {
 	spaceship.y = 240;
 	spaceship.h = 50;
 	spaceship.w = 80;
+	
+	shot.x = 1000;
+	shot.y = 1000;
+	shot.w = 20;
+	shot.h = 10;
 
 	window = SDL_CreateWindow("MyAwesomeGame", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH,SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE); //(position,position,size,size,flag)
 	if (window == NULL) {
@@ -48,12 +59,19 @@ int main(int argc, char *argv[]) {
 	}
 
 	render = SDL_CreateRenderer(window, -1, 0);
-	loading_surface = IMG_Load("Assets/background.png");
+	loading_surface = IMG_Load("Assets/images/background.png");
 	SDL_Texture *background_text = SDL_CreateTextureFromSurface(render, loading_surface);
 	SDL_FreeSurface(loading_surface);
-	loading_surface = IMG_Load("Assets/spaceship.png");
+	loading_surface = IMG_Load("Assets/images/spaceship.png");
 	SDL_Texture *spaceship_text = SDL_CreateTextureFromSurface(render, loading_surface);
 	SDL_FreeSurface(loading_surface);
+	loading_surface = IMG_Load("Assets/images/laser.png");
+	SDL_Texture *shot_texture = SDL_CreateTextureFromSurface(render, loading_surface);
+	SDL_FreeSurface(loading_surface);
+	bacground_music = Mix_LoadMUS("Assets/sound/background_music.ogg");
+	laser_beam = Mix_LoadWAV("Assets/sound/laser_beam.wav");
+
+	Mix_PlayMusic(bacground_music, -1);
 
 	while (1) {
 		SDL_Event event; //we create an event
@@ -75,8 +93,7 @@ int main(int argc, char *argv[]) {
 					player_input.pressing_left = true;
 					break;
 				case SDLK_SPACE:
-					//sx = x + 45;
-					//sy = y + 25;
+					player_input.pressing_space = true;
 					break;
 				case SDLK_ESCAPE:
 					return 0;
@@ -99,8 +116,7 @@ int main(int argc, char *argv[]) {
 					player_input.pressing_left = false;
 					break;
 				case SDLK_SPACE:
-					//sx = sx;
-					//sy = sy;
+					player_input.pressing_space = false;
 					break;
 				case SDLK_ESCAPE:
 					break;
@@ -126,18 +142,26 @@ int main(int argc, char *argv[]) {
 		{
 			spaceship.x -= speed;
 		}
-		//sx = sx + shotspeed;
-		//SDL_SetRenderDrawColor(render, 0, 0, 100, 255); // we set the color of the render
+		if (player_input.pressing_space ==true)
+		{
+			shot.x = spaceship.x+spaceship.w;
+			shot.y = spaceship.y+spaceship.h/2;
+			Mix_PlayChannel(-1, laser_beam, 0);
+		}
+		shot.x += shotspeed;
 		SDL_Delay(2);
 		SDL_RenderClear(render);
 		SDL_RenderCopy(render, background_text, NULL, NULL);
 		SDL_RenderCopy(render, spaceship_text, NULL, &spaceship);
+		SDL_RenderCopy(render, shot_texture, NULL, &shot);
 		SDL_RenderPresent(render);
 	}
 	SDL_DestroyWindow(window);
 	SDL_DestroyTexture(background_text);
 	SDL_DestroyTexture(spaceship_text);
 	Mix_CloseAudio();
+	Mix_FreeMusic(bacground_music);
+	Mix_FreeChunk(laser_beam);
 	Mix_Quit();
 	IMG_Quit();
 	SDL_Quit();
